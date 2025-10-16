28c0827480eca42f72662ae3a6502f06e9df7d7d
lchen <lchen@wolferesearch.com>
2025-10-16 01:18:09 +0000

    Changed color theme, added nvim-R

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,14 @@
 return {
   -- Install the Nord theme plugin
   { "shaunsingh/nord.nvim" },
+  { "AlexvZyl/nordic.nvim" },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 
   -- Tell LazyVim to use it
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nord",
+      colorscheme = "catppuccin",
     },
   },
 }
