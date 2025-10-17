03e0264412877fcd97977bcf1c874d1fb38386db
lchen <bowhead.whale.93@hotmail.com>
2025-10-17 12:09:57 -0400

    Fixed iron.nvim behavior to send python indented blocks

--- lua/plugins/iron.lua
+++ lua/plugins/iron.lua
@@ -4,20 +4,11 @@ return {
     config = function()
       local iron = require("iron.core")
 
-      -- Function to get Python command based on version
+      -- Function to get Python command from venv or system
       local function get_python_command()
-        -- Check if we're using Python 3.13+
-        local handle = io.popen("python3 --version 2>&1")
-        local version_output = handle:read("*a")
-        handle:close()
-
-        local major, minor = version_output:match("Python (%d+)%.(%d+)")
-        major = tonumber(major)
-        minor = tonumber(minor)
-
-        -- For Python 3.13+, use PYTHON_BASIC_REPL to avoid double indentation
-        if major and minor and (major > 3 or (major == 3 and minor >= 13)) then
-          return { "env", "PYTHON_BASIC_REPL=1", "python3" }
+        local venv = os.getenv("VIRTUAL_ENV")
+        if venv then
+          return { venv .. "/bin/python" }
         else
           return { "python3" }
         end
@@ -26,7 +17,10 @@ return {
       iron.setup({
         config = {
           repl_definition = {
-            python = { command = get_python_command() },
+            python = {
+              command = get_python_command(),
+              format = require("iron.fts.common").bracketed_paste,
+            },
             r = { command = { "R", "--no-save", "--no-restore", "--interactive" } },
           },
           repl_open_cmd = "botright 15 split",
@@ -45,7 +39,6 @@ return {
       -- (2) Send visual selection + go after block
       vim.keymap.set("v", "<leader>rl", function()
         iron.visual_send()
-        iron.send(nil, "\n") -- Extra newline to execute indented blocks
         -- move cursor to end of selection, then go one line down
         vim.cmd("normal! `>j")
       end, { desc = "Send selection and move down" })
@@ -53,7 +46,6 @@ return {
       -- (3) Send paragraph (handles indented blocks better)
       vim.keymap.set("n", "<leader>rp", function()
         iron.send_paragraph()
-        iron.send(nil, "\n") -- Extra newline to execute indented blocks
         vim.cmd("normal! }j") -- move to next paragraph
       end, { desc = "Send paragraph and move to next" })
     end,
